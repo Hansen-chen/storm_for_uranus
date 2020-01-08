7220f83e3750d9472b98443e1cd07bb7b6a508ae
Hansen <hansenchan1995@gmail.com>
2020-01-08 14:47:00 +0800

    add output

--- examples/storm-starter/src/jvm/org/apache/storm/starter/FastWordCountTopology.java
+++ examples/storm-starter/src/jvm/org/apache/storm/starter/FastWordCountTopology.java
@@ -14,6 +14,8 @@ package org.apache.storm.starter;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.Collection;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import org.apache.storm.Config;
@@ -44,49 +46,6 @@ import org.apache.storm.utils.Utils;
  */
 public class FastWordCountTopology {
 
-    public static void printMetrics(Nimbus.Iface client, String name) throws Exception {
-        ClusterSummary summary = client.getClusterInfo();
-        String id = null;
-        for (TopologySummary ts : summary.get_topologies()) {
-            if (name.equals(ts.get_name())) {
-                id = ts.get_id();
-            }
-        }
-        if (id == null) {
-            throw new Exception("Could not find a topology named " + name);
-        }
-        TopologyInfo info = client.getTopologyInfo(id);
-        int uptime = info.get_uptime_secs();
-        long acked = 0;
-        long failed = 0;
-        double weightedAvgTotal = 0.0;
-        for (ExecutorSummary exec : info.get_executors()) {
-            if ("spout".equals(exec.get_component_id())) {
-                SpoutStats stats = exec.get_stats().get_specific().get_spout();
-                Map<String, Long> failedMap = stats.get_failed().get(":all-time");
-                Map<String, Long> ackedMap = stats.get_acked().get(":all-time");
-                Map<String, Double> avgLatMap = stats.get_complete_ms_avg().get(":all-time");
-                for (String key : ackedMap.keySet()) {
-                    if (failedMap != null) {
-                        Long tmp = failedMap.get(key);
-                        if (tmp != null) {
-                            failed += tmp;
-                        }
-                    }
-                    long ackVal = ackedMap.get(key);
-                    double latVal = avgLatMap.get(key) * ackVal;
-                    acked += ackVal;
-                    weightedAvgTotal += latVal;
-                }
-            }
-        }
-        double avgLatency = weightedAvgTotal / acked;
-        System.out.println("uptime: " + uptime
-                + " acked: " + acked
-                + " avgLatency: " + avgLatency
-                + " acked/sec: " + (((double) acked) / uptime + " failed: " + failed));
-    }
-
     public static void kill(Nimbus.Iface client, String name) throws Exception {
         KillOptions opts = new KillOptions();
         opts.set_wait_secs(0);
@@ -110,18 +69,13 @@ public class FastWordCountTopology {
             name = args[0];
         }
 
-        conf.setNumWorkers(1);
+        conf.setNumWorkers(3);
         StormSubmitter.submitTopologyWithProgressBar(name, conf, builder.createTopology());
 
         Map<String, Object> clusterConf = Utils.readStormConfig();
         clusterConf.putAll(Utils.readCommandLineOpts());
         Nimbus.Iface client = NimbusClient.getConfiguredClient(clusterConf).getClient();
 
-        //Sleep for 5 mins
-        for (int i = 0; i < 10; i++) {
-            Thread.sleep(30 * 1000);
-            printMetrics(client, name);
-        }
         kill(client, name);
     }
 
@@ -162,6 +116,8 @@ public class FastWordCountTopology {
         public void declareOutputFields(OutputFieldsDeclarer declarer) {
             declarer.declare(new Fields("sentence"));
         }
+
+
     }
 
     public static class SplitSentence extends BaseBasicBolt {
@@ -197,6 +153,18 @@ public class FastWordCountTopology {
         @Override
         public void declareOutputFields(OutputFieldsDeclarer declarer) {
             declarer.declare(new Fields("word", "count"));
+        }
+
+        @Override
+        public void cleanup() {
+            System.out.println("--- FINAL COUNTS ---");
+            List<String> keys = new ArrayList<String>();
+            keys.addAll(this.counts.keySet());
+            Collections.sort(keys);
+            for (String key : keys) {
+                System.out.println(key + " : " + this.counts.get(key));
+            }
+            System.out.println("--------------");
         }
     }
 }
