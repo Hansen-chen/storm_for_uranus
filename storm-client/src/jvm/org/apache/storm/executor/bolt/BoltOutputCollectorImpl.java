853b78af288d70c2bbec22e9e69478f81f6054c4
Hansen <hansenchan1995@gmail.com>
2020-03-01 17:19:04 +0800

    fix: add one more variable

--- storm-client/src/jvm/org/apache/storm/executor/bolt/BoltOutputCollectorImpl.java
+++ storm-client/src/jvm/org/apache/storm/executor/bolt/BoltOutputCollectorImpl.java
@@ -82,7 +82,7 @@ public class BoltOutputCollectorImpl implements IOutputCollector {
     }
 
     @IntelSGXOcall
-    public static void annotated_emit(String streamId, Collection<Tuple> anchors, List<Object> values, List<Integer> outTasks, boolean ackingEnabled, Random random, BoltExecutor executor, int taskId, ExecutorTransfer xsfer, boolean isEventLoggers) {
+    public static void annotated_emit(String streamId, Collection<Tuple> anchors, List<Object> values, Task task, List<Integer> outTasks, boolean ackingEnabled, Random random, BoltExecutor executor, int taskId, ExecutorTransfer xsfer, boolean isEventLoggers) {
 
 
         for (int i = 0; i < outTasks.size(); ++i) {
@@ -133,6 +133,7 @@ public class BoltOutputCollectorImpl implements IOutputCollector {
                     (String)Tools.deep_copy(streamId),
                     (Collection<Tuple>)Tools.deep_copy(anchors),
                     (List<Object>)Tools.deep_copy(values),
+                    (Task)Tools.deep_copy(task),
                     (List<Integer>)Tools.deep_copy(outTasks),
                     ackingEnabled,
                     (Random)Tools.deep_copy(random),
