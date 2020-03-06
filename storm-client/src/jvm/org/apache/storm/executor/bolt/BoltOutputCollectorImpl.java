d9d817b3ea5e8eabf138fa57c89b166da1b28c78
Hansen <hansenchan1995@gmail.com>
2020-03-07 00:28:31 +0800

    minor fix

--- storm-client/src/jvm/org/apache/storm/executor/bolt/BoltOutputCollectorImpl.java
+++ storm-client/src/jvm/org/apache/storm/executor/bolt/BoltOutputCollectorImpl.java
@@ -80,7 +80,7 @@ public class BoltOutputCollectorImpl implements IOutputCollector {
                         xsfer,
                         isEventLoggers
                 );
-                return (List<Integer>)Tools.deep_copy(outTasks);;
+                return (List<Integer>)Tools.deep_copy(outTasks);
 
             }
             catch (UnsatisfiedLinkError ex){
