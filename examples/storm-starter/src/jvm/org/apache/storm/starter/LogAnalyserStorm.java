2be40d3cf145d292dfcd2aef93e606c52243e67a
Hansen <hansenchan1995@gmail.com>
2020-03-09 19:47:18 +0800

    example fix

--- examples/storm-starter/src/jvm/org/apache/storm/starter/LogAnalyserStorm.java
+++ examples/storm-starter/src/jvm/org/apache/storm/starter/LogAnalyserStorm.java
@@ -21,17 +21,21 @@ public class LogAnalyserStorm extends ConfigurableTopology {
     protected int run(String[] args) throws Exception {
         //Create Config instance for cluster configuration
         conf.setDebug(true);
-        conf.registerMetricsConsumer(org.apache.storm.metric.LoggingMetricsConsumer.class);
-        conf.setStatsSampleRate(1.0d);
+        conf.registerMetricsConsumer(org.apache.storm.metric.LoggingMetricsConsumer.class,1);
+        //conf.setStatsSampleRate(1.0d);
+        conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 50);
+        conf.put(Config.TOPOLOGY_MESSAGE_TIMEOUT_SECS, 60);
+        conf.put(Config.TOPOLOGY_ACKER_EXECUTORS, 5);
+
 
 
         TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("call-log-reader-spout", new FakeCallLogReaderSpout());
+        builder.setSpout("call-log-reader-spout", new FakeCallLogReaderSpout(), 1);
 
-        builder.setBolt("call-log-creator-bolt", new CallLogCreatorBolt())
+        builder.setBolt("call-log-creator-bolt", new CallLogCreatorBolt(), 1)
                 .shuffleGrouping("call-log-reader-spout");
 
-        builder.setBolt("call-log-counter-bolt", new CallLogCounterBolt())
+        builder.setBolt("call-log-counter-bolt", new CallLogCounterBolt(), 1)
                 .fieldsGrouping("call-log-creator-bolt", new Fields("call"));
 
         return submit("LogAnalyserStorm", conf, builder);
